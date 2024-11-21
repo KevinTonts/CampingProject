d9cbf750d8cf71aff750f1d2f05500282cfa47a9
kevin <kevintonts@gmail.com>
2024-11-21 16:26:37 +0200

    Display author of the review, hide review delete button for others and add middleware to check if is owner of the review

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,7 +4,7 @@ const catchAsync = require("../helpers/catchAsync");
 const Campground = require("../models/campground");
 const Review = require("../models/review");
 const ExpressError = require("../helpers/expressError");
-const {isLoggedIn, validateReview} = require("../middleware");
+const {isLoggedIn, validateReview, isReviewAuthor} = require("../middleware");
 
 router.post("/", isLoggedIn, validateReview, catchAsync(async (req, res) =>{
     const campground = await Campground.findById(req.params.id);
@@ -17,7 +17,7 @@ router.post("/", isLoggedIn, validateReview, catchAsync(async (req, res) =>{
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
-router.delete("/:reviewId",catchAsync( async (req, res) => {
+router.delete("/:reviewId", isLoggedIn, isReviewAuthor, catchAsync( async (req, res) => {
     const {id, reviewId} = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId }});
     await Review.findByIdAndDelete(reviewId);
