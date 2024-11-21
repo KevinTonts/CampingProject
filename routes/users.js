157d307eeafebc6e7494fc66f3febc24171da9d4
kevin <kevintonts@gmail.com>
2024-11-21 14:10:38 +0200

    Keep user logged in after registration

--- routes/users.js
+++ routes/users.js
@@ -8,13 +8,16 @@ router.get("/register", (req, res) => {
     res.render("users/register");
 })
 
-router.post("/register", catchAsync(async (req, res) =>{
+router.post("/register", catchAsync(async (req, res, next) =>{
     try {
         const {email, password, username} = req.body;
         const user = new User({email, username});
         const registerUser = await User.register(user, password);
-        req.flash('success', "Successfully registered!");
-        res.redirect("/campgrounds");
+        req.login(registerUser, err => {
+            if (err) return next(err);
+            req.flash('success', "Successfully registered!");
+            res.redirect("/campgrounds");
+        })
     } catch (e){
         req.flash('error', e.message);
         res.redirect("/register");
