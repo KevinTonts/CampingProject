8dab4ebc7ea5b2cfddf8d608df24720ad423f395
kevin <kevintonts@gmail.com>
2024-12-10 12:40:11 +0200

    Change data seed and refacto register form

--- seeds/index.js
+++ seeds/index.js
@@ -21,23 +21,23 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
+        const random100 = Math.floor(Math.random() * 100);
         const price = Math.floor(Math.random() * 30) + 10;
         const camp = new Campground({
             author: "673f12b97ce39d3256758c12", //your user id
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            location: `${cities[random100].city}, ${cities[random100].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: `Just something important`,
             price,
             geometry: {
                 type: "Point",
                 coordinates: [
-                    cities[random1000].longitude,
-                    cities[random1000].latitude
+                    cities[random100].longitude,
+                    cities[random100].latitude
                 ]
             },
             images: {
-                url: 'https://res.cloudinary.com/df5mssvlf/image/upload/v1732504745/CampingApp/jvp30w30gi5olpjnsfit.png',
+                url: 'https://res.cloudinary.com/df5mssvlf/image/upload/v1733826583/CampingApp/fei1cunmzy1vlds1rwum.jpg',
                 filename: 'CampingApp/jvp30w30gi5olpjnsfit',
             }
 
